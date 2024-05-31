0f897704f15078eda0c6c23e9e0d2b5cfe1d13e7
Rishank-Kashyap <167917876+Rishank-Kashyap@users.noreply.github.com>
2024-05-31 19:35:06 +0530

    commit

---
+++ Background Color Changer/script.js
@@ -0,0 +1,28 @@
+const randomColor = function() {
+    const hex = "01234567889ABCDEF"
+    let color='#'
+    for(let i=0; i<6; i++){
+        color += hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+
+let changeColor
+const startChangingColor = function() {
+    if(!changeColor){
+        changeColor = setInterval(changeBgColor, 1000)
+    }
+
+    function changeBgColor() {
+        document.body.style.backgroundColor = randomColor()
+    }
+}
+
+const stopChangingColor = function() {
+    clearInterval(changeColor)
+    changeColor = null
+}
+
+document.querySelector('#start').addEventListener('click', startChangingColor)
+
+document.querySelector('#stop').addEventListener('click', stopChangingColor)
